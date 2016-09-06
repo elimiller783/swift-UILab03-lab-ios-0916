9ca0f8087050ccfe6762a385be498cbbc2f6d79c
elimiller783 <emiller783@gmail.com>
2016-09-06 00:50:38 -0400

    ahh sookie sookie now

--- Cards/Cards/ViewController.swift
+++ Cards/Cards/ViewController.swift
@@ -11,9 +11,38 @@ import UIKit
 class ViewController: UIViewController {
     // TODO: Create IB outlets
 
+    @IBOutlet weak var suitDenominationUp: UILabel!
+    @IBOutlet weak var suitDenominationLow: UILabel!
+    @IBOutlet weak var largeNum: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     // TODO: IB actions and code to update UI
+    @IBAction func club(sender: AnyObject) {
+        largeNum.text = "4"
+        suitDenominationLow.text = "♣️"
+        suitDenominationUp.text = "♣️"
+    }
+
+    @IBAction func spade(sender: AnyObject) {
+        largeNum.text = "3"
+        suitDenominationLow.text = "♠️"
+        suitDenominationUp.text = "♣️"
+    }
+    
+    @IBAction func diamond(sender: AnyObject) {
+        largeNum.text = "8"
+        suitDenominationLow.text = "♦️"
+        suitDenominationUp.text = "♦️"
+    }
+    
+    @IBAction func heart(sender: AnyObject) {
+        largeNum.text = "10"
+        suitDenominationLow.text = "♥️"
+        suitDenominationUp.text = "♥️"
+    }
+    
+
 }
